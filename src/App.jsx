7253c03f11d7a3aad2b6e5795b756f7bf9a9b51c
Muhammad Notfaji <mohammadnoutfaji1234@gmail.com>
2025-04-15 12:54:19 +0200

    Created the section for the languages/chips

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
 import React from 'react'
+import { languages } from './languages'
+
+const languageElements = languages.map(lang => {
+  const styles = {
+    backgroundColor: lang.backgroundColor, 
+    color: lang.color}
+  return (
+    <p 
+      className='lang'
+      key={lang.name}
+      style={styles}
+    >
+      {lang.name}
+    </p>
+  )
+})
 
 export default function AssemblyEndgame() {
   return (
-      <main>
-          <header>
-              <h1>Assembly: Endgame</h1>
-              <p>Guess the word within 8 attempts to keep the 
-              programming world safe from Assembly!</p>
-              <div className='game-status'>
-                <h2>You win!</h2>
-                <p>Well done! 🎉</p>
-              </div>
-          </header>
-      </main>
+    <main>
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>Guess the word within 8 attempts to keep the 
+        programming world safe from Assembly!</p>
+        <div className='game-status'>
+          <h2>You win!</h2>
+          <p>Well done! 🎉</p>
+          </div>
+      </header>
+      <section className='languages-container'>
+        {languageElements}
+      </section>
+    </main>
   )
 }
