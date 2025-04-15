e211a7efa27368fd223d1ba3fea8484ba189204a
Muhammad Notfaji <mohammadnoutfaji1234@gmail.com>
2025-04-15 10:24:33 +0200

    Added game status section

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,10 @@ export default function AssemblyEndgame() {
               <h1>Assembly: Endgame</h1>
               <p>Guess the word within 8 attempts to keep the 
               programming world safe from Assembly!</p>
+              <div className='game-status'>
+                <h2>You win!</h2>
+                <p>Well done! 🎉</p>
+              </div>
           </header>
       </main>
   )
